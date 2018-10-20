39ab18b37a49042643efc6f7ff60cde1900b5c87
Abhishek Sharma <hello.abhishek@outlook.com>
2018-10-20 13:43:17 +0530

    add login screen

--- frontend/src/components/Login/presentational/Register.js
+++ frontend/src/components/Login/presentational/Register.js
@@ -2,32 +2,27 @@ import React, { Component } from "react";
 import { css } from "emotion";
 import FacebookLogin from "react-facebook-login";
 
-import bicycleimage from "../../../assets/bicycle_full.png";
-import locationimage from "../../../assets/location.png";
+import headerImage from "../../../assets/header.png";
+import locationimage from "../../../assets/logo.svg";
 
 class Register extends Component {
   render() {
     return (
       <div className={styles}>
-        <img className="bicycle_full" alt="bicycleimage" src={bicycleimage} />
-        <span>
-          <img
-            className="Combined-Shape"
-            alt="locationimage"
-            src={locationimage}
-          />
-          <span className="heyr">heyr</span>
-        </span>
-        <div />
+        <div className="header">
+          <img alt="loaderImage" src={headerImage} />
+        </div>
+        <img className="logo" alt="Heyr" src={locationimage} />
+        <h1>Easily Catch up with Like Minded People Nearby</h1>
         <FacebookLogin
           appId="193448981230282"
           autoLoad={true}
           fields="name,email,picture"
           callback={this.props.login}
         />
-        <span className="Easily-Catch-up-with">
-          Easily Catch up with Like Minded People Nearby
-        </span>
+        <p>
+          By continuing, you agree to our terms and conditons
+        </p>
       </div>
     );
   }
@@ -38,37 +33,47 @@ const styles = css`
   display: flex;
   flex-direction: column;
   align-items: center;
-  .bicycle_full {
-    width: 414px;
-    height: 312px;
-    mix-blend-mode: multiply;
+  padding:0 1.5em 1em 1.5em;
+  max-height: 100vh;
+  overflow:hidden;
+  .header{ 
+    img{
+      max-width: 100%;
+      margin: 10vh 0;
+    }
   }
-  .Combined-Shape {
-    width: 39.1px;
-    height: 57.1px;
+
+  height: 100vh;
+  overflow: auto;
+  .logo{
+    height:75px;
   }
-  .heyr {
-    width: 162.1px;
-    height: 75px;
-    color: #ff0086;
-  }
-  .Easily-Catch-up-with {
-    width: 376px;
-    height: 22px;
-    font-family: NunitoSans;
-    font-size: 16px;
-    font-weight: bold;
-    font-style: normal;
-    font-stretch: normal;
-    line-height: normal;
-    letter-spacing: normal;
-    text-align: center;
+  h1{
+    font-family: "Nunito Sans";
+    font-size: 1em;
     color: #000000;
+    font-weight: 700;
+    text-align: center;
+    margin-top:1em;
+    margin-bottom:4em;
+    line-height:1.5em;
   }
-  .Rectangle {
-    width: 326px;
-    height: 51px;
+  .kep-login-facebook{
+    font-family: "ProductSans-Bold";
+    text-transform: none;
+    font-size: 1em;
+    color: #FFFFFF;
+    letter-spacing: 0.5px;
     border-radius: 2px;
-    background-color: #3b5998;
+    border: 1px solid transparent;
+    text-align: center;
+  }
+  p{
+    margin-top: 1.5em;
+    font-family: "NunitoSans-Regular";
+    font-size: 14px;
+    color: #B1B1B1;
+    text-align: center;
+    line-height:1.25em;
   }
 `;
