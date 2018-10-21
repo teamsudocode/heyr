47cee00505527b4d51eb9cfc1f9141f2b06b8e4a
Madhurjya <migom6@gmail.com>
2018-10-21 07:29:32 +0530

    fix assignment to nil map

--- backend/src/routes.go
+++ backend/src/routes.go
@@ -14,7 +14,7 @@ import (
 var DB *mgo.Database
 
 // swipes made by a user
-var Swipes map[string]([]string)
+var Swipes map[string]([]string){}
 
 func NewRouter() *gin.Engine {
 	router := gin.Default()
@@ -60,11 +60,8 @@ func MarkSwipe(c *gin.Context) {
 		})
 		return
 	}
-	if _, there := Swipes[reqJson.UserID]; there {
-		Swipes[reqJson.UserID] = append(Swipes[reqJson.UserID], reqJson.MarkFor)
-	} else {
-		Swipes[reqJson.UserID] = []string{reqJson.MarkFor}
-	}
+
+	Swipes[reqJson.UserID] = append(Swipes[reqJson.UserID], reqJson.MarkFor)
 
 	c.JSON(http.StatusOK, gin.H{"message": "Done"})
 }
